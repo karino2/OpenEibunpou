8d52b6979d8af25e47680b3103eb33e2454288b0
karino2 <hogeika2@gmailcom>
2015-11-15 19:08:38 +0900

    Factor out UserPost related code to UserPostListHolder.

--- app/src/main/java/com/livejournal/karino2/openeibunpou/AnswerActivity.java
+++ app/src/main/java/com/livejournal/karino2/openeibunpou/AnswerActivity.java
@@ -16,50 +16,20 @@ import android.widget.TextView;
 public class AnswerActivity extends AppCompatActivity {
     final int USER_POST_ACTIVITY_ID = 1;
 
-    SimpleCursorAdapter adapter;
-    Database database;
-
     String stageName;
 
-    Sync sync;
-
-    View.OnClickListener likeTvClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            View par = (View)v.getParent();
-            long id = (long)v.getTag();
-            TextView likeTv2 = (TextView)v;
-            TextView dislikeTv2 =  (TextView)par.findViewById(R.id.textViewDislike);
-            onLikeClicked(id, par, likeTv2, dislikeTv2);
-        }
-    };
-
-    View.OnClickListener dislikeTvClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            View par = (View)v.getParent();
-            TextView dislikeTv2 = (TextView)v;
-            TextView likeTv2 =  (TextView)par.findViewById(R.id.textViewLike);
-            long id = (long) likeTv2.getTag();
-            onDislikeClicked(id, par, likeTv2, dislikeTv2);
-        }
-    };
-
     Sync getSync() {
-        if(sync == null)
-            sync = Sync.getInstance(this);
-        return sync;
+        return  Sync.getInstance(this);
     }
 
     QuestionRecord question;
+
+    UserPostListHolder postListHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
-
-        database = Database.getInstance(this);
-        sync = getSync();
 
         Intent intent = getIntent();
         if(intent != null) {
@@ -68,7 +38,7 @@ public class AnswerActivity extends AppCompatActivity {
 
             stageName = intent.getStringExtra("stageName");
 
-            setTitle(String.valueOf(question.getCompletion()+ " %"));
+            setTitle(String.valueOf(question.getCompletion() + " %"));
 
 
             TextView tv = (TextView)findViewById(R.id.textViewBody);
@@ -104,6 +74,7 @@ public class AnswerActivity extends AppCompatActivity {
             tv.setText(buf.toString());
 
         }
+        postListHolder = new UserPostListHolder(this, getSync(), Database.getInstance(this), stageName, question.getSubName());
 
 
         findViewById(R.id.buttonNext).setOnClickListener(new View.OnClickListener() {
@@ -114,62 +85,10 @@ public class AnswerActivity extends AppCompatActivity {
             }
         });
 
-        adapter = new SimpleCursorAdapter(this, R.layout.post_item, null,
-                new String[]{"body", "like", "dislike", "mylike", "owner"},
-                new int[]{ R.id.textViewPost, R.id.textViewLikeTotal, R.id.textViewDislikeTotal, R.id.textViewLike, R.id.textViewOwner });
-
-        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
-            @Override
-            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-                if(columnIndex == 5) {
-                    // view is testViewLike. set serverId here for a while.
-                    view.setTag(cursor.getLong(1));
-
-                    View parent = (View)view.getParent();
-                    int myval =cursor.getInt(5);
-                    parent.setTag(myval);
-                    TextView likeTv = (TextView)parent.findViewById(R.id.textViewLike);
-                    TextView dislikeTv = (TextView)parent.findViewById(R.id.textViewDislike);
-                    likeTv.setTextColor(Color.LTGRAY);
-                    dislikeTv.setTextColor(Color.LTGRAY);
-
-                    likeTv.setOnClickListener(likeTvClickListener);
-                    dislikeTv.setOnClickListener(dislikeTvClickListener);
-
-                    if(myval == 1) {
-                        likeTv.setTextColor(Color.RED);
-                    } else if (myval == -1) {
-                        dislikeTv.setTextColor(Color.RED);
-                    }
-                    return true;
-                }
-                return false;
-            }
-        });
         ListView lv = (ListView)findViewById(R.id.listViewComment);
-        lv.setAdapter(adapter);
+        lv.setAdapter(postListHolder.getAdapter());
 
-        getLoaderManager().initLoader(0, null, new LoaderManager.LoaderCallbacks<Cursor>() {
-            @Override
-            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-                return new SimpleCursorLoader(AnswerActivity.this) {
-                    @Override
-                    public Cursor loadCursor() {
-                        return database.queryPosts(stageName, question.getSubName());
-                    }
-                };
-            }
-
-            @Override
-            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-                adapter.swapCursor(data);
-            }
-
-            @Override
-            public void onLoaderReset(Loader<Cursor> loader) {
-                adapter.swapCursor(null);
-            }
-        });
+        getLoaderManager().initLoader(0, null, postListHolder.createLoaderCallbacks());
 
         findViewById(R.id.buttonNewPost).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -223,35 +142,6 @@ public class AnswerActivity extends AppCompatActivity {
     protected void onStop() {
         getSync().removeOnUserPostArrivedListener(R.layout.activity_answer);
         super.onStop();
-    }
-
-    public void onDislikeClicked(long id, View parentView, TextView likeTv, TextView dislikeTv) {
-        int current =(Integer) parentView.getTag();
-        if(current == -1) {
-            parentView.setTag(0);
-            likeTv.setTextColor(Color.LTGRAY);
-            sync.postLike(id, 0);
-            return;
-        }
-        parentView.setTag(-1);
-        dislikeTv.setTextColor(Color.RED);
-        likeTv.setTextColor(Color.LTGRAY);
-        sync.postLike(id, -1);
-        return;
-    }
-
-    public void onLikeClicked(long id, View parentView, TextView likeTv, TextView dislikeTv) {
-        int current =(Integer) parentView.getTag();
-        if(current == 1) {
-            parentView.setTag(0);
-            likeTv.setTextColor(Color.LTGRAY);
-            sync.postLike(id, 0);
-            return;
-        }
-        parentView.setTag(1);
-        likeTv.setTextColor(Color.RED);
-        dislikeTv.setTextColor(Color.LTGRAY);
-        sync.postLike(id, 1);
     }
 
     public static boolean isCorrect(int[] selected, int[] answer) {
---
+++ app/src/main/java/com/livejournal/karino2/openeibunpou/UserPostListHolder.java
@@ -0,0 +1,148 @@
+package com.livejournal.karino2.openeibunpou;
+
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by _ on 2015/11/15.
+ */
+public class UserPostListHolder {
+    Activity owner;
+    Sync sync;
+    Database database;
+    String stageName;
+    String subName;
+
+
+    SimpleCursorAdapter adapter;
+
+    public UserPostListHolder(Activity owner, Sync sync, Database database, String stageName, String subName) {
+        this.owner = owner;
+        this.sync = sync;
+        this.database = database;
+        this.stageName = stageName;
+        this.subName = subName;
+
+        adapter = new SimpleCursorAdapter(owner, R.layout.post_item, null,
+                new String[]{"body", "like", "dislike", "mylike", "owner"},
+                new int[]{ R.id.textViewPost, R.id.textViewLikeTotal, R.id.textViewDislikeTotal, R.id.textViewLike, R.id.textViewOwner });
+
+        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if(columnIndex == 5) {
+                    // view is testViewLike. set serverId here for a while.
+                    view.setTag(cursor.getLong(1));
+
+                    View parent = (View)view.getParent();
+                    int myval =cursor.getInt(5);
+                    parent.setTag(myval);
+                    TextView likeTv = (TextView)parent.findViewById(R.id.textViewLike);
+                    TextView dislikeTv = (TextView)parent.findViewById(R.id.textViewDislike);
+                    likeTv.setTextColor(Color.LTGRAY);
+                    dislikeTv.setTextColor(Color.LTGRAY);
+
+                    likeTv.setOnClickListener(likeTvClickListener);
+                    dislikeTv.setOnClickListener(dislikeTvClickListener);
+
+                    if(myval == 1) {
+                        likeTv.setTextColor(Color.RED);
+                    } else if (myval == -1) {
+                        dislikeTv.setTextColor(Color.RED);
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+
+    public SimpleCursorAdapter getAdapter() { return adapter; }
+    View.OnClickListener likeTvClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            View par = (View)v.getParent();
+            long id = (long)v.getTag();
+            TextView likeTv2 = (TextView)v;
+            TextView dislikeTv2 =  (TextView)par.findViewById(R.id.textViewDislike);
+            onLikeClicked(id, par, likeTv2, dislikeTv2);
+        }
+    };
+
+    View.OnClickListener dislikeTvClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            View par = (View)v.getParent();
+            TextView dislikeTv2 = (TextView)v;
+            TextView likeTv2 =  (TextView)par.findViewById(R.id.textViewLike);
+            long id = (long) likeTv2.getTag();
+            onDislikeClicked(id, par, likeTv2, dislikeTv2);
+        }
+    };
+
+
+    public LoaderManager.LoaderCallbacks<Cursor> createLoaderCallbacks() {
+        return new LoaderManager.LoaderCallbacks<Cursor>() {
+            @Override
+            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                return new SimpleCursorLoader(owner) {
+                    @Override
+                    public Cursor loadCursor() {
+                        return database.queryPosts(stageName, subName);
+                    }
+                };
+            }
+
+            @Override
+            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+                adapter.swapCursor(data);
+            }
+
+            @Override
+            public void onLoaderReset(Loader<Cursor> loader) {
+                adapter.swapCursor(null);
+            }
+        };
+    }
+
+    void onDislikeClicked(long id, View parentView, TextView likeTv, TextView dislikeTv) {
+        int current =(Integer) parentView.getTag();
+        if(current == -1) {
+            parentView.setTag(0);
+            likeTv.setTextColor(Color.LTGRAY);
+            sync.postLike(id, 0);
+            return;
+        }
+        parentView.setTag(-1);
+        dislikeTv.setTextColor(Color.RED);
+        likeTv.setTextColor(Color.LTGRAY);
+        sync.postLike(id, -1);
+        return;
+    }
+
+    void onLikeClicked(long id, View parentView, TextView likeTv, TextView dislikeTv) {
+        int current =(Integer) parentView.getTag();
+        if(current == 1) {
+            parentView.setTag(0);
+            likeTv.setTextColor(Color.LTGRAY);
+            sync.postLike(id, 0);
+            return;
+        }
+        parentView.setTag(1);
+        likeTv.setTextColor(Color.RED);
+        dislikeTv.setTextColor(Color.LTGRAY);
+        sync.postLike(id, 1);
+    }
+
+
+
+}
